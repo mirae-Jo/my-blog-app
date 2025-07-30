c5446043f896dc461ff0588fc0742aa51007cb65
mirae <mirae9873@gmail.com>
2025-07-30 19:22:14 +0900

    style: tailwindcss/typography prose(마크다운 문법 테마) 적용 후 code 블록만 prism - coy 테마 적용하기(SyntaxHighlighter)

--- src/components/SelectedPost.tsx
+++ src/components/SelectedPost.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { coy } from "react-syntax-highlighter/dist/esm/styles/prism";
+
 import remarkBreaks from "remark-breaks";
 import type { Post } from "../context/PostContext";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { coy } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 interface SelectedPostProps {
   post: Post;
@@ -24,42 +25,11 @@ const SelectedPost: React.FC<SelectedPostProps> = ({ post }) => {
           {post.title}
         </h4>
       </div>
-      <div className='p-10'>
+      <div className='p-10 prose max-w-none'>
         <ReactMarkdown
           remarkPlugins={[remarkBreaks]}
           components={{
-            h1: ({ node, ...props }) => (
-              <h1 className='text-4xl font-bold my-4' {...props} />
-            ),
-            h2: ({ node, ...props }) => (
-              <h2 className='text-3xl font-bold my-4' {...props} />
-            ),
-            h3: ({ node, ...props }) => (
-              <h3 className='text-2xl font-bold my-3' {...props} />
-            ),
-            h4: ({ node, ...props }) => (
-              <h4 className='text-xl font-bold my-3' {...props} />
-            ),
-            h5: ({ node, ...props }) => (
-              <h5 className='text-lg font-bold my-2' {...props} />
-            ),
-            h6: ({ node, ...props }) => (
-              <h6 className='text-base font-bold my-2' {...props} />
-            ),
-            blockquote: ({ node, ...props }) => (
-              <blockquote
-                className='border-l-4 border-gray-300 pl-4 italic my-4'
-                {...props}
-              />
-            ),
-            p: ({ node, ...props }) => <p className='mb-4.5' {...props} />,
-            ul: ({ node, ...props }) => (
-              <ul className='list-disc pl-5 mb-4' {...props} />
-            ),
-            ol: ({ node, ...props }) => (
-              <ol className='list-decimal pl-5 mb-4' {...props} />
-            ),
-            li: ({ node, ...props }) => <li className='mb-2' {...props} />,
+            pre: (props) => <pre className='not-prose' {...props} />,
             code: ({
               node,
               inline,
