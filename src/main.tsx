9396dc2c7aba1d1a14352d603b0effdff209b6de
mirae <mirae9873@gmail.com>
2025-12-24 15:21:28 +0900

    fix: strict mode 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import { PostProvider } from './context/PostContext';
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { PostProvider } from "./context/PostContext";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <PostProvider>
-          <App />
-        </PostProvider>
-      </AuthProvider>
-    </BrowserRouter>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <PostProvider>
+        <App />
+      </PostProvider>
+    </AuthProvider>
+  </BrowserRouter>
+);
